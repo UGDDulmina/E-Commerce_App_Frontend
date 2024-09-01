6bb55f3ec319d556b1ebda2a6d43769f0db949d9
UGDDulmina <ug.dinuja@gmail.com>
2024-09-01 13:43:34 +0530

    Create login page for buyer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import LoginAsBuyer from './components/LoginAsBuyer';
 import LoginHome from './components/loginHome'
 import React from 'react';
 
 function App() {
   return (
     <>
-       <LoginHome/>
+       {/* <LoginHome/> */}
+       <LoginAsBuyer/>
        
     </>
   )
--- src/components/LoginHome.jsx
+++ src/components/LoginHome.jsx
@@ -5,15 +5,14 @@ export default function loginHome() {
 
     <div>
     <div className="flex flex-col items-center justify-center min-h-screen">
-    <h2 className="text-4xl font-bold mb-6 text-gray-500">Join Our Platform as a Buyer or Seller</h2>  
-
+    <h2 className="text-4xl font-bold mb-6 text-gray-700">Join Our Platform as a Buyer or Seller</h2>  
     <div className="flex flex-col md:flex-row w-full items-center justify-center">
      <div className="w-full md:w-1/4 p-4">
       <div className="text-center mb-3 p-6 border-2 border-green-500 rounded-lg shadow-md">
         <h5 className="text-lg font-bold"></h5>
-        <p className="text-gray-600 text-xl">"I’m a seller, offering high-quality goods for buyer needs."</p>
+        <p className="text-gray-600 text-xl">"I’ m a seller, offering high-quality goods for buyer needs."</p>
         <button href="#" className="inline-block mt-3 px-4 py-2 text-lg font-semibold text-green-700 border-2 border-green-600 rounded hover:bg-green-500 hover:text-white transition duration-1000 ease-in-out">
-          Signup as seller
+         Signup as seller
         </button>
       </div>
     </div>
@@ -21,14 +20,17 @@ export default function loginHome() {
     <div className="w-full md:w-1/4 p-4">
       <div className="text-center mb-3 p-6 border-2 border-green-500 rounded-lg shadow-md">
         <h5 className="text-lg font-bold"></h5>
-        <p className="text-gray-600 text-xl">"I’m a buyer, looking for high-quality goods for my needs."</p>
+        <p className="text-gray-600 text-xl">"I’ m a buyer, looking for high-quality goods for my needs."</p>
         <button href="#" className="inline-block mt-3 px-4 py-2 text-lg font-semibold text-green-700 border-2 border-green-600 rounded hover:bg-green-500 hover:text-white transition duration-1000 ease-in-out">
-          Signup as buyer
+        Signup as buyer
         </button>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
+    <div>
+     <p className='text-lg'>Already have an account? <a href="" className='text-green-600 text-xl'> Log in</a></p> 
+    </div>
+   </div>
   </div>
   )
 }
